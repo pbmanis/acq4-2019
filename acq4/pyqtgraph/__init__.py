9fb21604f6e6d9fcae8620b92bff4c37cd785952
Paul manis <pmanis@med.unc.edu>
2019-01-04 13:44:59 -0500

    Just use os.closerange to close file descriptors in all systems.

--- acq4/pyqtgraph/__init__.py
+++ acq4/pyqtgraph/__init__.py
@@ -367,15 +367,9 @@ def exit():
     atexit._run_exitfuncs()
     
     ## close file handles
-    if sys.platform == 'darwin':
-        for fd in range(3, 4096):
-            if fd not in [7]:  # trying to close 7 produces an illegal instruction on the Mac.
-                os.close(fd)
-    else:
-        os.closerange(3, 4096) ## just guessing on the maximum descriptor count..
-
+    os.closerange(3, 4096) ## just guessing on the maximum descriptor count..
     os._exit(0)
-    
+
 
 
 ## Convenience functions for command-line use
