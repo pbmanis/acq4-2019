e9b342aa7e69f7a333413a9968f761b0e259cf82
Paul B. Manis <pmanis@med.unc.edu>
2020-03-19 16:58:05 -0400

    fixing deprecated time call (for python 3.8)

--- acq4/pyqtgraph/ptime.py
+++ acq4/pyqtgraph/ptime.py
@@ -7,23 +7,28 @@ Distributed under MIT/X11 license. See license.txt for more infomation.
 
 
 import sys
-import time as systime
+#import time as systime
+import timeit  # Python 3.8 deprecates time
 START_TIME = None
 time = None
 
 def winTime():
     """Return the current time in seconds with high precision (windows version, use Manager.time() to stay platform independent)."""
-    return systime.clock() + START_TIME
+#    return systime.clock() + START_TIME
     #return systime.time()
+    return timeit.default_timer()
 
 def unixTime():
     """Return the current time in seconds with high precision (unix version, use Manager.time() to stay platform independent)."""
-    return systime.time()
+    #return systime.time()
+    return timeit.default_timer()
 
 if sys.platform.startswith('win'):
-    cstart = systime.clock()  ### Required to start the clock in windows
-    START_TIME = systime.time() - cstart
-    
+    # cstart = systime.clock()  ### Required to start the clock in windows
+    # START_TIME = systime.time() - cstart
+    cstart =  timeit.default_timer()
+    START_TIME = timeit.default_timer() - cstart
+
     time = winTime
 else:
     time = unixTime
