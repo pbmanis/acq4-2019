93824c7bef92c87c0ef3499126fec28e853577f4
Paul B. Manis <pmanis@med.unc.edu>
2020-03-19 16:57:28 -0400

    trying batch installs

---
+++ installpipenv.bat
@@ -0,0 +1,7 @@
+pip3 install --upgrade pip
+pip3 install --user virtualenv
+python3 -m venv env
+source env/bin/activate
+pip3 install --upgrade pip 
+pip3 install requests
+pip3 install matplotlib numpy scipy pyparsing==2.0.3 pyserial pyqt5 lmfit h5py six pillow
---
+++ installpipenv.sh
@@ -0,0 +1,8 @@
+# install required modules ec. with pip and a virtual environment
+pip3 install --upgrade pip
+pip3 install --user virtualenv
+python3 -m venv env
+source env/bin/activate
+pip3 install --upgrade pip  # do this inside envronment as well. 
+pip3 install requests
+pip3 install matplotlib numpy scipy pyparsing==2.0.3 pyserial pyqt5 lmfit h5py six pillow
